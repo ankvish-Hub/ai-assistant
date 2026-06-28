da90905eb7b719efbf715604401d6832bbea3f09
Ankit Vishwakarma <ankvish1102@gmail.com>
2026-06-28 22:47:29 +0530

    first commit

---
+++ server/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+const authRoutes = require("./routes/authRoutes");
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
+module.exports = app;
